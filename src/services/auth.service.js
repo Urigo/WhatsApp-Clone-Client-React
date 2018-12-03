94e6f00e2005e59905c07f77095d42b02e863ab5
DAB0mB <emanor6@gmail.com>
2018-12-03 15:41:56 +0800

    Use fetch hook instead of native fetch API

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Redirect } from 'react-router-dom'
+import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 export const withAuth(component: React.Component) => {
   return (props) => getAuthHeader() ? (
--- src/services/fetch.service.js
+++ src/services/fetch.service.js
@@ -1,22 +1,20 @@
-import auth from './auth.service'
+import useFetch from 'fetch-suspense';
+import { getAuthHeader } from './auth.service';
 
 // Will automatically append necessary data to params
 export default (url, metadata) => {
   if (!url.includes('.')) {
-    if (/^\//) {
-      url = `http://${process.env.REACT_APP_APOLLO_SERVER_URI}${url}`
-    }
-    else {
-      url = `http://${process.env.REACT_APP_APOLLO_SERVER_URI}/${url}`
-    }
+    url = /^\//.test(url)
+      ? `http://${process.env.REACT_APP_APOLLO_SERVER_URI}${url}`
+      : `http://${process.env.REACT_APP_APOLLO_SERVER_URI}/${url}`;
   }
 
-  const auth = auth.getAuthHeader();
+  const auth = getAuthHeader();
 
   if (auth) {
-    metadata = Object.assign({}, metadata)
-    metadata.headers = Object.assign({}, auth, headers)
+    metadata = Object.assign({}, metadata);
+    metadata.headers = Object.assign({}, auth, headers);
   }
 
-  return fetch(...args)
-}
+  return useFetch(...args);
+};
