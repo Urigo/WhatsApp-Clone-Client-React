c9bdeb56ba9e4cd3009e7592f851664d5e2f4544
DAB0mB <emanor6@gmail.com>
2018-12-03 15:27:42 +0800

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import * as React from 'react';
-import './App.scss';
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { withAuth } from './services/auth.service';
 
-import logo from './logo.svg';
-
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default () => (
+  <Switch>
+    <Route exact path='/auth' component={AuthScreen} />
+    <Route exact path='/chats' component={ChatsListScreen} />
+    <Route exact path='/chats/:chatId' component={ChatScreen} />
+    <Route exact path='/new-chat' component={NewChatScreen} />
+    <Route exact path='/new-chat/group' component={NewChatGroupScreen} />
+    <Route path='/' to="/chats" component={Redirect} />
+  </Switch>
+)
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,17 +1,34 @@
+import React from 'react'
+import { Redirect } from 'react-router-dom'
+
+export const withAuth(component: React.Component) => {
+  return (props) => getAuthHeader() ? (
+    <Component {...props} authHeader={getAuthHeader()} user={getUser()} />
+  ) : (
+    <Redirect {...props} to="/login" />
+  );
+};
+
+export const storeAuthHeader = (auth: string) => {
+  localStorage.setItem('Authorization', auth);
+};
+
+export const getAuthHeader = (): string => {
+  return localStorage.getItem('Authorization');
+};
+
+export const storeUser = (user: User) => {
+  localStorage.setItem('user', JSON.stringify(user));
+};
+
+export const getUser = (): User => {
+  return JSON.parse(localStorage.getItem('user'));
+};
+
 export default {
-  storeAuthHeader(auth: string) {
-    localStorage.setItem('Authorization', auth);
-  }
-
-  getAuthHeader(): string {
-    return localStorage.getItem('Authorization');
-  }
-
-  storeUser(user: User) {
-    localStorage.setItem('user', JSON.stringify(user));
-  }
-
-  getUser(): User {
-    return JSON.parse(localStorage.getItem('user'));
-  }
-}
+  withAuth,
+  storeAuthHeader,
+  getAuthHeader,
+  storeUser,
+  getUser,
+};
