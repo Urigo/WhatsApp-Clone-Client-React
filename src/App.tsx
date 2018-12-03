1bd6ff5c2d35438c964c41e9f5a1f67c030a12e2
DAB0mB <emanor6@gmail.com>
2018-12-03 15:31:29 +0800

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { withAuth } from './services/auth.service';
+import AuthScreen from './screens/AuthScreen';
+import ChatsListScreen from './screens/ChatsListScreen';
+import ChatScreen from './screens/ChatScreen';
+import NewChatScreen from './screens/NewChatScreen';
+import NewChatGroupScreen from './screens/NewChatGroupScreen';
 
 export default () => (
   <Switch>
-    <Route exact path='/auth' component={AuthScreen} />
-    <Route exact path='/chats' component={ChatsListScreen} />
-    <Route exact path='/chats/:chatId' component={ChatScreen} />
-    <Route exact path='/new-chat' component={NewChatScreen} />
-    <Route exact path='/new-chat/group' component={NewChatGroupScreen} />
-    <Route path='/' to="/chats" component={Redirect} />
+    <Route exact path="/auth" component={AuthScreen} />
+    <Route exact path="/chats" component={ChatsListScreen} />
+    <Route exact path="/chats/:chatId" component={ChatScreen} />
+    <Route exact path="/new-chat" component={NewChatScreen} />
+    <Route exact path="/new-chat/group" component={NewChatGroupScreen} />
+    <Route path="/" to="/chats" component={Redirect} />
   </Switch>
 )
