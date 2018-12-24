3890593f1910281cd1069cb6231dd17571fc4124
DAB0mB <emanor6@gmail.com>
2018-12-24 23:05:55 +0800

    Use message added subscription

--- src/graphql-hooks/messages-hooks.ts
+++ src/graphql-hooks/messages-hooks.ts
@@ -1,7 +1,83 @@
-import { useQuery, useMutation } from 'react-apollo-hooks'
-import { addMessageMutation } from '../graphql-documents/messages-documents'
-import { AddMessage } from '../types'
+import { useEffect } from 'react'
+import { useQuery, useMutation, MutationUpdaterFn } from 'react-apollo-hooks'
+import { time as uniqid } from 'uniqid'
+import store from '../apollo-client'
+import { getChatsQuery, getChatQuery } from '../graphql-documents/chats-documents'
+import { addMessageMutation, messageAddedSubscription } from '../graphql-documents/messages-documents'
+import { getMeQuery } from '../graphql-documents/users-documents'
+import { useSubscription } from '../polyfills/react-apollo-hooks'
+import { AddMessage, GetChat, GetChats } from '../types'
 
-export const useAddMessage = (options?) => {
-  return useMutation<AddMessage.Mutation, AddMessage.Variables>(addMessageMutation, options)
+export const useAddMessage = (options: {
+  variables: AddMessage.Variables,
+  [key: string]: any,
+}) => {
+  const { data: { messageAdded } } = useSubscription(messageAddedSubscription)
+  const { data: { me } } = useQuery(getMeQuery)
+  const { chatId, content } = options.variables
+
+  useEffect(() => {
+    if (!messageAdded) return
+
+    {
+      const { chat }: GetChat.Query = store.readQuery({
+        query: getChatQuery,
+        variables: { chatId },
+      })
+
+      chat.messages.push(messageAdded)
+
+      store.writeQuery({
+        query: getChatQuery,
+        variables: { chatId },
+        data: { chat },
+      })
+    }
+
+    {
+      const { chats }: GetChats.Query = store.readQuery({
+        query: getChatsQuery,
+      })
+
+      const chat = chats.find(chat => chat.id === chatId)
+
+      if (chat) {
+        chat.messages.push(messageAdded)
+
+        store.writeQuery({
+          query: getChatsQuery,
+          data: { chats },
+        })
+      }
+    }
+  }, [messageAdded])
+
+  return useMutation<AddMessage.Mutation, AddMessage.Variables>(addMessageMutation, {
+    variables: {
+      chatId,
+      content,
+    },
+    optimisticResponse: {
+      __typename: 'Mutation',
+      addMessage: {
+        id: uniqid(),
+        __typename: 'Message',
+        chat: {
+          id: chatId,
+          __typename: 'Chat',
+        },
+        sender: {
+          id: me.id,
+          __typename: 'User',
+          name: me.name,
+        },
+        content,
+        createdAt: new Date(),
+        type: 0,
+        recipients: [],
+        ownership: true,
+      },
+    },
+    ...options,
+  })
 }
