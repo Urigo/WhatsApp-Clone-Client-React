3ee25aca63329cf9c7f78b7e98c1cbc9493d8b39
DAB0mB <emanor6@gmail.com>
2019-01-15 13:16:29 +0800

    Remove redundant introspection

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -1,4 +1,4 @@
-import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloClient } from 'apollo-client'
 import { ApolloLink, split } from 'apollo-link'
 import { setContext } from 'apollo-link-context'
@@ -6,7 +6,6 @@ import { HttpLink } from 'apollo-link-http'
 import { WebSocketLink } from 'apollo-link-ws'
 import { getMainDefinition } from 'apollo-utilities'
 import { OperationDefinitionNode } from 'graphql'
-import * as introspectionQueryResultData from './graphql/introspection.json'
 import { getAuthHeader } from './services/auth.service'
 
 const httpUri = process.env.REACT_APP_SERVER_URL + '/graphql'
@@ -48,13 +47,7 @@ const terminatingLink = split(
 
 const link = ApolloLink.from([terminatingLink])
 
-const fragmentMatcher = new IntrospectionFragmentMatcher({
-  introspectionQueryResultData,
-})
-
-const cache = new InMemoryCache({
-  fragmentMatcher,
-})
+const cache = new InMemoryCache()
 
 export default new ApolloClient({
   link,
