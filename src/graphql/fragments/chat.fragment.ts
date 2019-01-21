44c7e48b4b52a2c42e6ae4f76129e94d8d1cfb0a
DAB0mB <emanor6@gmail.com>
2019-01-21 20:57:49 +0800

    Step 1.6: Add ChatsListScreen

---
+++ src/graphql/fragments/chat.fragment.ts
@@ -0,0 +1,22 @@
+import gql from 'graphql-tag'
+import message from './message.fragment'
+
+export default gql `
+  fragment Chat on Chat {
+    id
+    name
+    picture
+    allTimeMembers {
+      id
+      name
+      picture
+    }
+    owner {
+      id
+    }
+    lastMessage {
+      ...Message
+    }
+  }
+  ${message}
+`
