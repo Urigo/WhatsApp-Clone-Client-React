a7a2a414e52a7bc4578e24506d6fdb380d64da64
DAB0mB <emanor6@gmail.com>
2019-01-21 17:20:02 +0800

    Step 1.3: Setup Apollo-client

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo-hooks';
 import './index.css';
 import App from './App';
+import apolloClient from './apollo-client'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={apolloClient}>
+    <App />
+  </ApolloProvider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
