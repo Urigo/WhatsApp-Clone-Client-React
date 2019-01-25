a6a8602767bf237074f5bce02a13b869378edeb7
DAB0mB <emanor6@gmail.com>
2019-01-25 14:50:20 +0800

    Step 2.7: Make components navigatable

--- src/components/ChatsListScreen/index.tsx
+++ src/components/ChatsListScreen/index.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import { Suspense } from 'react'
+import { RouteComponentProps } from 'react-router-dom'
 import Navbar from '../Navbar'
 import ChatsList from './ChatsList'
 import ChatsNavbar from './ChatsNavbar'
 
-export default () => (
+export default ({ history }: RouteComponentProps) => (
   <div className="ChatsListScreen Screen">
     <Navbar>
-      <ChatsNavbar />
+      <ChatsNavbar history={history} />
     </Navbar>
     <Suspense fallback={null}>
       <ChatsList />
