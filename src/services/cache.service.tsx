6e5bfc1fa975f975cc8fb2ecc93ea74dde38c2df
DAB0mB <emanor6@gmail.com>
2019-01-26 20:48:55 +0800

    Step 3.1: Add chat room screen

--- src/services/cache.service.tsx
+++ src/services/cache.service.tsx
@@ -1,7 +1,8 @@
 import { defaultDataIdFromObject } from 'apollo-cache-inmemory'
 import * as fragments from '../graphql/fragments'
 import * as subscriptions from '../graphql/subscriptions'
-import { ChatUpdated } from '../graphql/types'
+import * as queries from '../graphql/queries'
+import { ChatUpdated, MessageAdded, Message, Chats, FullChat } from '../graphql/types'
 import { useSubscription } from '../polyfills/react-apollo-hooks'
 
 export const useSubscriptions = () => {
@@ -13,6 +14,56 @@ export const useSubscriptions = () => {
         fragmentName: 'Chat',
         data: chatUpdated,
       })
+    },
+  })
+
+  useSubscription<MessageAdded.Subscription>(subscriptions.messageAdded, {
+    onSubscriptionData: ({ client, subscriptionData: { messageAdded } }) => {
+      client.writeFragment<Message.Fragment>({
+        id: defaultDataIdFromObject(messageAdded),
+        fragment: fragments.message,
+        data: messageAdded,
+      })
+
+      let fullChat
+      try {
+        fullChat = client.readFragment<FullChat.Fragment>({
+          id: defaultDataIdFromObject(messageAdded.chat),
+          fragment: fragments.fullChat,
+          fragmentName: 'FullChat',
+        })
+      } catch (e) {}
+
+      if (fullChat && !fullChat.messages.some(message => message.id === messageAdded.id)) {
+        fullChat.messages.push(messageAdded)
+        fullChat.lastMessage = messageAdded
+
+        client.writeFragment({
+          id: defaultDataIdFromObject(fullChat),
+          fragment: fragments.fullChat,
+          fragmentName: 'FullChat',
+          data: fullChat,
+        })
+      }
+
+      let chats
+      try {
+        chats = client.readQuery<Chats.Query>({
+          query: queries.chats,
+        }).chats
+      } catch (e) {}
+
+      if (chats) {
+        const index = chats.findIndex(chat => chat.id === messageAdded.chat.id)
+        const chat = chats[index]
+        chats.splice(index, 1)
+        chats.unshift(chat)
+
+        client.writeQuery({
+          query: queries.chats,
+          data: { chats },
+        })
+      }
     },
   })
 }
