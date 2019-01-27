05db8234e2ebacc0aa5fbace1ddc447344f02025
DAB0mB <emanor6@gmail.com>
2019-01-27 21:45:17 +0800

    Step 4.4: Apply group logic to existing components

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ export default () => (
       <Route exact path="/new-chat" component={withAuth(NewChatScreen)} />
       <Route exact path="/new-chat/group" component={withAuth(NewGroupScreen)} />
       <Route exact path="/new-chat/group/details" component={withAuth(GroupDetailsScreen)} />
+      <Route exact path="/chats/:chatId/details" component={withAuth(GroupDetailsScreen)} />
       <Route component={RedirectToChats} />
     </AnimatedSwitch>
   </BrowserRouter>
