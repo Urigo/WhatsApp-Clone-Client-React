2a8d2b1d5c008ba9c92df0668ce86436edbac3a8
DAB0mB <emanor6@gmail.com>
2019-03-10 14:58:24 +0800

    Step 2.2: Setup Material-UI theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,25 @@
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#2c6157' },
+    secondary: { main: '#6fd056' },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+})
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
