fb636dbbf4729d8cd7e953647b4cc235afd3ce98
DAB0mB <emanor6@gmail.com>
2019-03-10 14:58:24 +0800

    Step 2.2: Setup Material-UI theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,22 @@
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#2c6157' },
+    secondary: { main: '#6fd056' },
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
