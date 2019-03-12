199f0c233407fd5a0a05b00ec7510b7990e91b93
DAB0mB <emanor6@gmail.com>
2019-03-13 01:32:27 +0800

    Step 5.2: Setup tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import { GlobalWithFetchMock } from 'jest-fetch-mock';
+import { act } from '@testing-library/react';
+
+const customGlobal: GlobalWithFetchMock = global as GlobalWithFetchMock;
+customGlobal.fetch = require('jest-fetch-mock');
+customGlobal.fetchMock = customGlobal.fetch;
