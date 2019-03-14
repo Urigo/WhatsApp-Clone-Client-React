16d68c56573dbeefdea15cd649d315ba126a5d19
DAB0mB <emanor6@gmail.com>
2019-03-14 15:23:27 +0800

    Step 5.1: Add data-testid attributes

--- src/components/ChatsListScreen/ChatsList.tsx
+++ src/components/ChatsListScreen/ChatsList.tsx
@@ -92,14 +92,20 @@ const ChatsList = () => {
     <Container>
       <StyledList>
         {chats.map((chat) => (
-          <StyledListItem key={chat!.id} button>
-            <ChatPicture src={chat.picture} alt="Profile" />
+          <StyledListItem key={chat.id} button>
+            <ChatPicture
+              data-testid="picture"
+              src={chat.picture}
+              alt="Profile"
+            />
             <ChatInfo>
-              <ChatName>{chat.name}</ChatName>
+              <ChatName data-testid="name">{chat.name}</ChatName>
               {chat.lastMessage && (
                 <React.Fragment>
-                  <MessageContent>{chat.lastMessage.content}</MessageContent>
-                  <MessageDate>
+                  <MessageContent data-testid="content">
+                    {chat.lastMessage.content}
+                  </MessageContent>
+                  <MessageDate data-testid="date">
                     {moment(chat.lastMessage.createdAt).format('HH:mm')}
                   </MessageDate>
                 </React.Fragment>
