598b46b508f24f61cdd246032e96da0126cdd366
DAB0mB <emanor6@gmail.com>
2019-03-18 23:25:33 +0800

    Step 6.1: Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import ChatRoomScreen from './components/ChatRoomScreen';
 import ChatsListScreen from './components/ChatsListScreen';
 
 const App: React.FC = () => (
-  <div>
-    <ChatsListScreen />
-  </div>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/chats" component={ChatsListScreen} />
+      <Route exact path="/chats/:chatId" component={ChatRoomScreen} />
+    </Switch>
+    <Route exact path="/" render={redirectToChats} />
+  </BrowserRouter>
 );
+
+const redirectToChats = () => <Redirect to="/chats" />;
 
 export default App;
