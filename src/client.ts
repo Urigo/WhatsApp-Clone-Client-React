65227c59c9a913158ea54d4015a6d091615631c8
DAB0mB <emanor6@gmail.com>
2019-03-27 01:25:31 +0800

    Step 10.1: Setup WS link

--- src/client.ts
+++ src/client.ts
@@ -1,16 +1,42 @@
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-client';
+import { getMainDefinition } from 'apollo-utilities';
 import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { ApolloLink, split } from 'apollo-link';
 
 const httpUri = process.env.REACT_APP_SERVER_URL + '/graphql';
+const wsUri = httpUri.replace(/^https?/, 'ws');
 
 const httpLink = new HttpLink({
   uri: httpUri,
 });
 
+const wsLink = new WebSocketLink({
+  uri: wsUri,
+  options: {
+    // Automatic reconnect in case of connection error
+    reconnect: true,
+  },
+});
+
+const terminatingLink = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    // If this is a subscription query, use wsLink, otherwise use httpLink
+    return (
+      kind === 'OperationDefinition' && operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+const link = ApolloLink.from([terminatingLink]);
+
 const inMemoryCache = new InMemoryCache();
 
 export default new ApolloClient({
-  link: httpLink,
+  link,
   cache: inMemoryCache,
 });
