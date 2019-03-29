6199a5fa424b05ad99756a0eba22f94e8726b26b
DAB0mB <emanor6@gmail.com>
2019-03-29 11:10:33 +0800

    Step 11.1: Distinguish messages

--- src/components/ChatsListScreen/ChatsList.test.tsx
+++ src/components/ChatsListScreen/ChatsList.test.tsx
@@ -40,6 +40,7 @@ describe('ChatsList', () => {
                   id: 1,
                   content: 'Hello',
                   createdAt: new Date('1 Jan 2019 GMT'),
+                  isMine: true,
                   chat: {
                     __typename: 'Chat',
                     id: 1,
@@ -90,6 +91,7 @@ describe('ChatsList', () => {
                   id: 1,
                   content: 'Hello',
                   createdAt: new Date('1 Jan 2019 GMT'),
+                  isMine: true,
                   chat: {
                     __typename: 'Chat',
                     id: 1,
