94ada26fd52cc6e698da983961d4b73ee34943e0
DAB0mB <emanor6@gmail.com>
2019-03-29 11:10:33 +0800

    Step 11.1: Distinguish messages

--- src/components/ChatRoomScreen/index.tsx
+++ src/components/ChatRoomScreen/index.tsx
@@ -70,6 +70,7 @@ const ChatRoomScreen: React.FC<ChatRoomScreenParams> = ({
               .toString(36)
               .substr(2, 9),
             createdAt: new Date(),
+            isMine: true,
             chat: {
               __typename: 'Chat',
               id: chatId,
