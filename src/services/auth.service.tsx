fdbe050a61ff20d08380642110f223acd595609f
DAB0mB <emanor6@gmail.com>
2019-04-07 17:16:31 +0800

    Step 13.1: Use authToken cookie

--- src/services/auth.service.tsx
+++ src/services/auth.service.tsx
@@ -23,8 +23,8 @@ export const withAuth = <P extends object>(Component: React.ComponentType<P>) =>
   };
 };
 
-export const signIn = (currentUserId: string) => {
-  document.cookie = `currentUserId=${currentUserId}`;
+export const signIn = (authToken: string) => {
+  document.cookie = `authToken=${authToken}`;
 
   // This will become async in the near future
   return Promise.resolve();
@@ -33,12 +33,12 @@ export const signIn = (currentUserId: string) => {
 export const signOut = () => {
   // "expires" represents the lifespan of a cookie. Beyond that date the cookie will
   // be deleted by the browser. "expires" cannot be viewed from "document.cookie"
-  document.cookie = `currentUserId=;expires=${new Date(0)}`;
+  document.cookie = `authToken=;expires=${new Date(0)}`;
 
   // Clear cache
   return client.clearStore();
 };
 
 export const isSignedIn = () => {
-  return /currentUserId=.+(;|$)/.test(document.cookie);
+  return /authToken=.+(;|$)/.test(document.cookie);
 };
