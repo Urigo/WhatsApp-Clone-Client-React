42cae65bfb1b7f175c5584346869d95d7943a414
DAB0mB <emanor6@gmail.com>
2019-04-09 00:37:31 +0800

    Step 12.1: Add basic ChatCreationScreen

---
+++ src/components/ChatsListScreen/AddChatButton.test.tsx
@@ -0,0 +1,27 @@
+import { createMemoryHistory } from 'history';
+import { ApolloProvider } from 'react-apollo-hooks';
+import React from 'react';
+import { cleanup, render, fireEvent, wait } from '@testing-library/react';
+import AddChatButton from './AddChatButton';
+import { mockApolloClient } from '../../test-helpers';
+
+describe('AddChatButton', () => {
+  afterEach(cleanup);
+
+  it('goes back on arrow click', async () => {
+    const history = createMemoryHistory();
+    const client = mockApolloClient();
+
+    {
+      const { container, getByTestId } = render(
+        <ApolloProvider client={client}>
+          <AddChatButton history={history} />
+        </ApolloProvider>
+      );
+
+      fireEvent.click(getByTestId('new-chat-button'));
+
+      await wait(() => expect(history.location.pathname).toEqual('/new-chat'));
+    }
+  });
+});
