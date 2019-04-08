7c5d57dc055a53955486c11479a2c8e5011ece92
DAB0mB <emanor6@gmail.com>
2019-04-08 12:41:27 +0800

    Step 13.5: Add signUp() method to auth.service

---
+++ src/graphql/mutations/signUp.mutation.ts
@@ -0,0 +1,9 @@
+import gql from 'graphql-tag'
+
+export default gql`
+  mutation signUp($name: String!, $username: String!, $password: String!, $passwordConfirm: String!) {
+    signUp(name: $name, username: $username, password: $password, passwordConfirm: $passwordConfirm) {
+      id
+    }
+  }
+`
