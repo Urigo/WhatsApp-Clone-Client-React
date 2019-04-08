9112fac573a2456d8956f8e5bc7c261531f891f2
DAB0mB <emanor6@gmail.com>
2019-04-08 12:41:27 +0800

    Step 13.5: Add signUp() method to auth.service

--- src/services/auth.service.tsx
+++ src/services/auth.service.tsx
@@ -62,6 +62,25 @@ export const signIn = ({ username, password }: { username: string, password: str
   });
 };
 
+export const signUp = ({ name, username, password, passwordConfirm }: 
+  {name: string, username: string, password: string, passwordConfirm: string}) => {
+  return client.mutate({
+    mutation: gql`
+      mutation signUp($name: String!, $username: String!, $password: String!, $passwordConfirm: String!) {
+        signUp(name: $name, username: $username, password: $password, passwordConfirm: $passwordConfirm) {
+          id
+        }
+      }
+    `,
+    variables: {
+      name,
+      username,
+      password,
+      passwordConfirm
+    }
+  });
+};
+
 export const signOut = () => {
   document.cookie = `authToken=;expires=${new Date(0)}`;
 
