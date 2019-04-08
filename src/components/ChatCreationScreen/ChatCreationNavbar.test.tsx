271ec6bf520b983719a248ffd076144c0a0909de
DAB0mB <emanor6@gmail.com>
2019-04-09 00:37:31 +0800

    Step 12.1: Add basic ChatCreationScreen

---
+++ src/components/ChatCreationScreen/ChatCreationNavbar.test.tsx
@@ -0,0 +1,26 @@
+import { createMemoryHistory } from 'history';
+import React from 'react';
+import { cleanup, render, fireEvent, wait } from '@testing-library/react';
+import ChatCreationNavbar from './ChatCreationNavbar';
+
+describe('ChatCreationNavbar', () => {
+  afterEach(cleanup);
+
+  it('goes back on arrow click', async () => {
+    const history = createMemoryHistory();
+
+    history.push('/new-chat');
+
+    await wait(() => expect(history.location.pathname).toEqual('/new-chat'));
+
+    {
+      const { container, getByTestId } = render(
+        <ChatCreationNavbar history={history} />
+      );
+
+      fireEvent.click(getByTestId('back-button'));
+
+      await wait(() => expect(history.location.pathname).toEqual('/chats'));
+    }
+  });
+});
