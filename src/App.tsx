64ac149039c4d157be461c6794716787ec401267
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-15 17:17:01 +0200

    Step 1.4: Move ChatsList to a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ChatsList from './components/ChatsList';
 
 const App: React.FC = () => {
   return (
@@ -6,22 +7,7 @@ const App: React.FC = () => {
       <div>
         Whatsapp Clone
       </div>
-      <div>
-        <ul>
-            <li>
-              <img src="https://randomuser.me/api/portraits/thumb/men/1.jpg" alt="Profile"/>
-              <div>Ethan Gonzalez</div>
-                <div>You on your way?</div>
-                <div>10:25</div>
-            </li>
-            <li>
-              <img src="https://randomuser.me/api/portraits/thumb/men/2.jpg" alt="Profile"/>
-              <div>Bryan Wallace</div>
-                <div>Hey, it's me</div>
-                <div>13:27</div>
-            </li>
-        </ul>
-      </div>
+      <ChatsList />
     </div>
   );
 };
