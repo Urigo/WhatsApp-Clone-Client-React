b8a8034a381234534cd8c5cbf65edc952d5b7d43
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-15 17:52:56 +0200

    Step 1.8: Attach unique keys to ChatsList

--- src/components/ChatsListScreen/ChatsList.tsx
+++ src/components/ChatsListScreen/ChatsList.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
+import { chats } from '../../db';
 
 const ChatsList: React.FC = () => (
   <div>
     <ul>
-      <li>
-        <img
-          src="https://randomuser.me/api/portraits/thumb/men/1.jpg"
-          alt="Profile"
-        />
-        <div>Ethan Gonzalez</div>
-        <div>You on your way?</div>
-        <div>10:25</div>
-      </li>
-      <li>
-        <img
-          src="https://randomuser.me/api/portraits/thumb/men/2.jpg"
-          alt="Profile"
-        />
-        <div>Bryan Wallace</div>
-        <div>Hey, it's me</div>
-        <div>13:27</div>
-      </li>
+      {chats.map((chat) => (
+        <li key={chat.id}>
+          <img src={chat.picture} alt="Profile" />
+          <div>{chat.name}</div>
+        </li>
+      ))}
     </ul>
   </div>
 );
