fe9b213c61bc237a952ab38e25bbe44531f087b5
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-16 19:00:59 +0200

    Step 2.5: Add style with styled-components

--- src/components/ChatsListScreen/ChatsList.tsx
+++ src/components/ChatsListScreen/ChatsList.tsx
@@ -2,24 +2,81 @@ import React from 'react';
 import { chats } from '../../db';
 import moment from 'moment';
 import { List, ListItem } from '@material-ui/core';
+import styled from 'styled-components';
 
-const ChatsList: React.FC = () => (
-  <div>
-    <List>
+const Container = styled.div`
+  height: calc(100% - 56px);
+  overflow-y: overlay;
+`;
+
+const StyledList = styled(List)`
+  padding: 0 !important;
+`;
+
+const StyledListItem = styled(ListItem)`
+  height: 76px;
+  padding: 0 15px;
+  display: flex;
+`;
+
+const ChatPicture = styled.img`
+  height: 50px;
+  width: 50px;
+  object-fit: cover;
+  border-radius: 50%;
+`;
+
+const ChatInfo = styled.div`
+  width: calc(100% - 60px);
+  height: 46px;
+  padding: 15px 0;
+  margin-left: 10px;
+  border-bottom: 0.5px solid silver;
+  position: relative;
+`;
+
+const ChatName = styled.div`
+  margin-top: 5px;
+`;
+
+const MessageContent = styled.div`
+  color: gray;
+  font-size: 15px;
+  margin-top: 5px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+`;
+
+const MessageDate = styled.div`
+  position: absolute;
+  color: gray;
+  top: 20px;
+  right: 0;
+  font-size: 13px;
+`;
+
+const ChatsList = () => (
+  <Container>
+    <StyledList>
       {chats.map((chat) => (
-        <ListItem key={chat.id} button>
-          <img src={chat.picture} alt="Profile" />
-          <div>{chat.name}</div>
-          {chat.lastMessage && (
-            <React.Fragment>
-              <div>{chat.lastMessage.content}</div>
-              <div>{moment(chat.lastMessage.createdAt).format('HH:mm')}</div>
-            </React.Fragment>
-          )}
-        </ListItem>
+        <StyledListItem key={chat.id} button>
+          <ChatPicture src={chat.picture} alt="Profile" />
+          <ChatInfo>
+            <ChatName>{chat.name}</ChatName>
+            {chat.lastMessage && (
+              <React.Fragment>
+                <MessageContent>{chat.lastMessage.content}</MessageContent>
+                <MessageDate>
+                  {moment(chat.lastMessage.createdAt).format('HH:mm')}
+                </MessageDate>
+              </React.Fragment>
+            )}
+          </ChatInfo>
+        </StyledListItem>
       ))}
-    </List>
-  </div>
+    </StyledList>
+  </Container>
 );
 
 export default ChatsList;
