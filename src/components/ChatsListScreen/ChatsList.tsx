9b0ed53ae62dcdbd2075a63ecfab771ddfc06587
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-16 18:34:37 +0200

    Step 2.4: Use Material components

--- src/components/ChatsListScreen/ChatsList.tsx
+++ src/components/ChatsListScreen/ChatsList.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { chats } from '../../db';
 import moment from 'moment';
+import { List, ListItem } from '@material-ui/core';
 
 const ChatsList: React.FC = () => (
   <div>
-    <ul>
+    <List>
       {chats.map((chat) => (
-        <li key={chat.id}>
+        <ListItem key={chat.id} button>
           <img src={chat.picture} alt="Profile" />
           <div>{chat.name}</div>
           {chat.lastMessage && (
@@ -15,9 +16,9 @@ const ChatsList: React.FC = () => (
               <div>{moment(chat.lastMessage.createdAt).format('HH:mm')}</div>
             </React.Fragment>
           )}
-        </li>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   </div>
 );
 
