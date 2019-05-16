691d6837fc7011d834278f1a1e901a9d70c2ff26
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-16 19:00:59 +0200

    Step 2.5: Add style with styled-components

--- src/components/ChatsListScreen/ChatsNavbar.tsx
+++ src/components/ChatsListScreen/ChatsNavbar.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { Toolbar } from '@material-ui/core';
+import styled from 'styled-components';
 
-const ChatsNavbar: React.FC = () => <Toolbar>Whatsapp Clone</Toolbar>;
+const Container = styled(Toolbar)`
+  background-color: var(--primary-bg);
+  color: var(--primary-text);
+  font-size: 20px;
+  line-height: 40px;
+`;
+
+const ChatsNavbar: React.FC = () => <Container>Whatsapp Clone</Container>;
 
 export default ChatsNavbar;
