24d35f42c7c2610a688dad3d5d67e873f981144f
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-21 21:34:57 +0300

    Step 6.4: Test new navigation logic

--- src/components/ChatsListScreen/ChatsList.test.tsx
+++ src/components/ChatsListScreen/ChatsList.test.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { cleanup, render, waitForDomChange } from '@testing-library/react';
+import {
+  cleanup,
+  render,
+  fireEvent,
+  wait,
+  waitForDomChange,
+} from '@testing-library/react';
 import ChatsList from './ChatsList';
+import { createBrowserHistory } from 'history';
 
 describe('ChatsList', () => {
-  afterEach(cleanup);
+  afterEach(() => {
+    cleanup();
+    delete window.location;
+
+    window.location = {
+        href: '/',
+    };
+  });
 
   it('renders fetched chats data', async () => {
     fetchMock.mockResponseOnce(
@@ -38,6 +52,41 @@ describe('ChatsList', () => {
       );
       expect(getByTestId('content')).toHaveTextContent('Hello');
       expect(getByTestId('date')).toHaveTextContent('02:00');
+    }
+  });
+
+  it('should navigate to the target chat room on chat item click', async () => {
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        data: {
+          chats: [
+            {
+              id: 1,
+              name: 'Foo Bar',
+              picture: 'https://localhost:4000/picture.jpg',
+              lastMessage: {
+                id: 1,
+                content: 'Hello',
+                createdAt: new Date('1 Jan 2019 GMT'),
+              },
+            },
+          ],
+        },
+      })
+    );
+
+    const history = createBrowserHistory();
+
+    {
+      const { container, getByTestId } = render(
+        <ChatsList history={history} />
+      );
+
+      await waitForDomChange({ container });
+
+      fireEvent.click(getByTestId('chat'));
+
+      await wait(() => expect(history.location.pathname).toEqual('/chats/1'));
     }
   });
 });
