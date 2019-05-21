6ee3acb3fd787160411c0ecf25f6d7d801fb192b
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-21 23:16:27 +0300

    Step 6.9: Test ChatRoomScreen child components

--- src/components/ChatRoomScreen/ChatNavbar.tsx
+++ src/components/ChatRoomScreen/ChatNavbar.tsx
@@ -47,11 +47,11 @@ const ChatNavbar: React.FC<ChatNavbarProps> = ({ chat, history }) => {
 
   return (
     <Container>
-      <BackButton onClick={navBack}>
+      <BackButton data-testid="back-button" onClick={navBack}>
         <ArrowBackIcon />
       </BackButton>
-      <Picture src={chat.picture} />
-      <Name>{chat.name}</Name>
+      <Picture data-testid="chat-picture" src={chat.picture} />
+      <Name data-testid="chat-name">{chat.name}</Name>
     </Container>
   );
 };
