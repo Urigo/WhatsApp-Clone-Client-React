97abdd2e2a6369b1aa0bf79d208514491fc72779
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-21 21:49:48 +0300

    Step 6.5: Animate route switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Route, Redirect, Switch, RouteComponentProps } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, RouteComponentProps } from 'react-router-dom';
 import ChatRoomScreen from './components/ChatRoomScreen';
 import ChatsListScreen from './components/ChatsListScreen';
+import AnimatedSwitch from './components/AnimatedSwitch';
 
 const App: React.FC = () => (
   <BrowserRouter>
-    <Switch>
+    <AnimatedSwitch>
       <Route exact path="/chats" component={ChatsListScreen} />
 
       <Route exact path="/chats/:chatId" component={
@@ -13,7 +14,7 @@ const App: React.FC = () => (
         (<ChatRoomScreen chatId={match.params.chatId} />)
       } />
 
-    </Switch>
+    </AnimatedSwitch>
     <Route exact path="/" render={redirectToChats} />
   </BrowserRouter>
 );
