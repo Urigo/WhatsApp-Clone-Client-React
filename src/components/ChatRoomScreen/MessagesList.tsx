50b24db5cfab2e2c67a72a8ce4709518dd588358
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-21 22:24:00 +0300

    Step 6.6: Implement ChatRoomScreen components

---
+++ src/components/ChatRoomScreen/MessagesList.tsx
@@ -0,0 +1,78 @@
+import moment from 'moment';
+import React from 'react';
+import styled from 'styled-components';
+import { ChatQueryMessage } from './index';
+
+const Container = styled.div`
+  display: block;
+  flex: 2;
+  overflow-y: overlay;
+  padding: 0 15px;
+`;
+
+const MessageItem = styled.div`
+  float: right;
+  background-color: #dcf8c6;
+  display: inline-block;
+  position: relative;
+  max-width: 100%;
+  border-radius: 7px;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.15);
+  margin-top: 10px;
+  margin-bottom: 10px;
+  clear: both;
+
+  &::after {
+    content: '';
+    display: table;
+    clear: both;
+  }
+
+  &::before {
+    background-image: url(/assets/message-mine.png);
+    content: '';
+    position: absolute;
+    bottom: 3px;
+    width: 12px;
+    height: 19px;
+    right: -11px;
+    background-position: 50% 50%;
+    background-repeat: no-repeat;
+    background-size: contain;
+  }
+`;
+
+const Contents = styled.div`
+  padding: 5px 7px;
+  word-wrap: break-word;
+
+  &::after {
+    content: ' \\00a0\\00a0\\00a0\\00a0\\00a0\\00a0\\00a0\\00a0\\00a0\\00a0\\00a0\\00a0\\00a0\\00a0\\00a0\\00a0\\00a0\\00a0\\00a0';
+    display: inline;
+  }
+`;
+
+const Timestamp = styled.div`
+  position: absolute;
+  bottom: 2px;
+  right: 7px;
+  color: gray;
+  font-size: 12px;
+`;
+
+interface MessagesListProps {
+  messages: Array<ChatQueryMessage>;
+}
+
+const MessagesList: React.FC<MessagesListProps> = ({ messages }) => (
+  <Container>
+    {messages.map((message: any) => (
+      <MessageItem key={message.id}>
+        <Contents>{message.content}</Contents>
+        <Timestamp>{moment(message.createdAt).format('HH:mm')}</Timestamp>
+      </MessageItem>
+    ))}
+  </Container>
+);
+
+export default MessagesList;
