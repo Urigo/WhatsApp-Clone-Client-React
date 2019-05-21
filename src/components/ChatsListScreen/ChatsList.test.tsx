03c4859023235387e78169273d62483737791ecb
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-21 21:34:57 +0300

    Step 6.4: Test new navigation logic

--- src/components/ChatsListScreen/ChatsList.test.tsx
+++ src/components/ChatsListScreen/ChatsList.test.tsx
@@ -1,12 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { cleanup, render, waitForDomChange } from '@testing-library/react';
+import {
+  cleanup,
+  render,
+  fireEvent,
+  wait,
+  waitForDomChange,
+} from '@testing-library/react';
 import ChatsList from './ChatsList';
+import { createBrowserHistory } from 'history';
 
 describe('ChatsList', () => {
-  afterEach(cleanup);
+  afterEach(() => {
+    cleanup();
+    window.location.pathname = '/';
+  });
 
   it('renders fetched chats data', async () => {
+    const time = new Date('1 Jan 2019 GMT');
+
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        data: {
+          chats: [
+            {
+              id: 1,
+              content: 'Hello',
+              createdAt: time.getTime(),
+            },
+          ],
+        },
+      })
+    );
+
+    {
+      const { container, getByTestId } = render(<ChatsList />);
+
+      await waitForDomChange({ container });
+
+      expect(getByTestId('name')).toHaveTextContent('Foo Bar');
+      expect(getByTestId('picture')).toHaveAttribute(
+        'src',
+        'https://localhost:4000/picture.jpg'
+      );
+      expect(getByTestId('content')).toHaveTextContent('Hello');
+      expect(getByTestId('date')).toHaveTextContent('10:00');
+    }
+  });
+
+  it('should navigate to the target chat room on chat item click', async () => {
     fetch.mockResponseOnce(
       JSON.stringify({
         data: {
@@ -26,18 +68,18 @@ describe('ChatsList', () => {
       })
     );
 
+    const history = createBrowserHistory();
+
     {
-      const { container, getByTestId } = render(<ChatsList />);
+      const { container, getByTestId } = render(
+        <ChatsList history={history} />
+      );
 
       await waitForDomChange({ container });
 
-      expect(getByTestId('name')).toHaveTextContent('Foo Bar');
-      expect(getByTestId('picture')).toHaveAttribute(
-        'src',
-        'https://localhost:4000/picture.jpg'
-      );
-      expect(getByTestId('content')).toHaveTextContent('Hello');
-      expect(getByTestId('date')).toHaveTextContent('01:00');
+      fireEvent.click(getByTestId('chat'));
+
+      await wait(() => expect(history.location.pathname).toEqual('/chats/1'));
     }
   });
 });
