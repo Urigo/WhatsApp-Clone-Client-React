f02468981420ed7816e6a535c06c6c46be6605fe
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-21 21:17:29 +0300

    Step 6.3: Navigate to chat on click

--- src/components/ChatsListScreen/index.tsx
+++ src/components/ChatsListScreen/index.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import ChatsNavbar from './ChatsNavbar';
 import ChatsList from './ChatsList';
 import styled from 'styled-components';
+import { History } from 'history';
 
 const Container = styled.div`
   height: 100vh;
 `;
 
-const ChatsListScreen: React.FC = () => (
+interface ChatsListScreenProps {
+  history: History;
+}
+
+const ChatsListScreen: React.FC<ChatsListScreenProps> = ({ history }) => (
   <Container>
     <ChatsNavbar />
-    <ChatsList />
+    <ChatsList history={history} />
   </Container>
 );
 
