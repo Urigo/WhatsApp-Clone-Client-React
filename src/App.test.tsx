1931e50a5f5f7880f6e8ecc141759af94998e80c
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-25 16:14:47 +0300

    Step 7.4: Mock Apollo requests in tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { mockApolloClient } from './test-helpers';
 
 it('renders without crashing', () => {
+  const client = mockApolloClient();
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
