c5e205c4655f13b96ae1efff866e08256ff6f39a
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-27 00:25:02 +0200

    Step 8.2: Rewrite lastMessage to chats query

--- src/components/ChatsListScreen/ChatsList.test.tsx
+++ src/components/ChatsListScreen/ChatsList.test.tsx
@@ -11,6 +11,7 @@ import {
 import { createBrowserHistory } from 'history';
 import { mockApolloClient } from '../../test-helpers';
 import ChatsList, { getChatsQuery } from './ChatsList';
+import * as queries from '../../graphql/queries';
 
 describe('ChatsList', () => {
   afterEach(() => {
@@ -25,7 +26,7 @@ describe('ChatsList', () => {
   it('renders fetched chats data', async () => {
     const client = mockApolloClient([
       {
-        request: { query: getChatsQuery },
+        request: { query: queries.chats },
         result: {
           data: {
             chats: [
@@ -71,7 +72,7 @@ describe('ChatsList', () => {
   it('should navigate to the target chat room on chat item click', async () => {
     const client = mockApolloClient([
       {
-        request: { query: getChatsQuery },
+        request: { query: queries.chats },
         result: {
           data: {
             chats: [
