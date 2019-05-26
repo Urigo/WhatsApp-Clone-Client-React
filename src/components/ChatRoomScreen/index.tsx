492fbe94a658aed274dc01833fac7ae133958228
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-27 00:55:35 +0200

    Step 8.4: Rewrite fragments

--- src/components/ChatRoomScreen/index.tsx
+++ src/components/ChatRoomScreen/index.tsx
@@ -1,3 +1,4 @@
+import { defaultDataIdFromObject } from 'apollo-cache-inmemory';
 import gql from 'graphql-tag';
 import React from 'react';
 import { useCallback } from 'react';
@@ -84,48 +85,75 @@ const ChatRoomScreen: React.FC<ChatRoomScreenParams> = ({
         },
         update: (client, { data }) => {
           if (data && data.addMessage) {
+            type FullChat = { [key: string]: any };
+            let fullChat;
+            const chatIdFromStore = defaultDataIdFromObject(chat);
+  
+            if (chatIdFromStore === null) {
+              return;
+            }
+  
+            try {
+              fullChat = client.readFragment<FullChat>({
+                id: chatIdFromStore,
+                fragment: fragments.fullChat,
+                fragmentName: 'FullChat',
+              });
+            } catch (e) {
+              return;
+            }
+  
+            if (fullChat === null || 
+                fullChat.messages === null ||
+                data === null ||
+                data.addMessage === null ||
+                data.addMessage.id === null) {
+              return;
+            }
+            if (fullChat.messages.some((currentMessage: any) => currentMessage.id === data.addMessage.id)){
+              return;
+            }
+  
+            fullChat.messages.push(data.addMessage);
+            fullChat.lastMessage = data.addMessage;
+  
+            client.writeFragment({
+              id: chatIdFromStore,
+              fragment: fragments.fullChat,
+              fragmentName: 'FullChat',
+              data: fullChat,
+            });
+
+            let clientChatsData;
+            try {
+              clientChatsData = client.readQuery<ChatsResult>({
+                query: queries.chats,
+              });
+            } catch (e) {
+              return;
+            }
+  
+            if (!clientChatsData || clientChatsData === null) {
+              return null;
+            }
+            if (!clientChatsData.chats || clientChatsData.chats === undefined) {
+              return null;
+            }
+            const chats = clientChatsData.chats;
+  
+            const chatIndex = chats.findIndex((currentChat: any) => currentChat.id === chatId);
+            if (chatIndex === -1) return;
+            const chatWhereAdded = chats[chatIndex];
+
+            // The chat will appear at the top of the ChatsList component
+            chats.splice(chatIndex, 1);
+            chats.unshift(chatWhereAdded);
+  
             client.writeQuery({
-              query: getChatQuery,
-              variables: { chatId },
-              data: {
-                chat: {
-                  ...chat,
-                  messages: chat.messages.concat(data.addMessage),
-                },
-              },
-            });
-          }
-
-          let clientChatsData;
-          try {
-            clientChatsData = client.readQuery<ChatsResult>({
               query: queries.chats,
+              data: { chats: chats },
             });
-          } catch (e) {
-            return;
           }
-
-          if (!clientChatsData || clientChatsData === null) {
-            return null;
-          }
-          if (!clientChatsData.chats || clientChatsData.chats === undefined) {
-            return null;
-          }
-          const chats = clientChatsData.chats;
-
-          const chatIndex = chats.findIndex((currentChat: any) => currentChat.id === chatId);
-          if (chatIndex === -1) return;
-          const chatWhereAdded = chats[chatIndex];
-
-          chatWhereAdded.lastMessage = data.addMessage;
-          // The chat will appear at the top of the ChatsList component
-          chats.splice(chatIndex, 1);
-          chats.unshift(chatWhereAdded);
-
-          client.writeQuery({
-            query: queries.chats,
-            data: { chats: chats },
-          });
         },
       });
     },
