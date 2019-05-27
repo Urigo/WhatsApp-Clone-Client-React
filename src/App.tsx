428fafce4bc4691ec298969fbee2ddb78d92bb7c
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-27 18:37:08 +0200

    Step 11.6: Add withAuth() route wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -9,32 +9,27 @@ import AuthScreen from './components/AuthScreen';
 import ChatRoomScreen from './components/ChatRoomScreen';
 import ChatsListScreen from './components/ChatsListScreen';
 import AnimatedSwitch from './components/AnimatedSwitch';
-import { useCacheService } from './services/cache.service';
+import { withAuth } from './services/auth.service';
 
-const App: React.FC = () => {
-  useCacheService();
+const App: React.FC = () => (
+  <BrowserRouter>
+    <AnimatedSwitch>
+      <Route exact path="/sign-(in|up)" component={AuthScreen} />
+      <Route exact path="/chats" component={withAuth(ChatsListScreen)} />
 
-  return (
-    <BrowserRouter>
-      <AnimatedSwitch>
-        <Route exact path="/sign-(in|up)" component={AuthScreen} />
-        <Route exact path="/chats" component={ChatsListScreen} />
-
-        <Route
-          exact
-          path="/chats/:chatId"
-          component={({
-            match,
-            history,
-          }: RouteComponentProps<{ chatId: string }>) => (
+      <Route
+        exact
+        path="/chats/:chatId"
+        component={withAuth(
+          ({ match, history }: RouteComponentProps<{ chatId: string }>) => (
             <ChatRoomScreen chatId={match.params.chatId} history={history} />
-          )}
-        />
-      </AnimatedSwitch>
-      <Route exact path="/" render={redirectToChats} />
-    </BrowserRouter>
-  );
-};
+          )
+        )}
+      />
+    </AnimatedSwitch>
+    <Route exact path="/" render={redirectToChats} />
+  </BrowserRouter>
+);
 
 const redirectToChats = () => <Redirect to="/chats" />;
 
