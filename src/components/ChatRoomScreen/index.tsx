18a92092c5b4f03de774aed441d1c89bc840dc29
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-27 17:14:59 +0200

    Step 10.4: Update cache on message added

--- src/components/ChatRoomScreen/index.tsx
+++ src/components/ChatRoomScreen/index.tsx
@@ -1,4 +1,3 @@
-import { defaultDataIdFromObject } from 'apollo-cache-inmemory';
 import gql from 'graphql-tag';
 import React from 'react';
 import { useCallback } from 'react';
@@ -8,8 +7,8 @@ import MessageInput from './MessageInput';
 import MessagesList from './MessagesList';
 import { History } from 'history';
 import { useGetChatQuery, useAddMessageMutation } from '../../graphql/types';
-import * as queries from '../../graphql/queries';
 import * as fragments from '../../graphql/fragments';
+import { writeMessage } from '../../services/cache.service';
 
  const Container = styled.div `
   background: url(/assets/chat-background.jpg);
@@ -42,10 +41,6 @@ interface ChatRoomScreenParams {
   chatId: string
   history: History;
 };
-  
-interface ChatsResult {
-  chats: any[];
-}
 
 const ChatRoomScreen: React.FC<ChatRoomScreenParams> = ({ history, chatId }) => {
   const { data, loading } = useGetChatQuery({
@@ -72,62 +67,9 @@ const ChatRoomScreen: React.FC<ChatRoomScreenParams> = ({ history, chatId }) => 
         }
       },
       update: (client, { data: { addMessage } }) => {
-        type FullChat = { [key: string]: any };
-        let fullChat;
-        const chatIdFromStore = defaultDataIdFromObject(chat);
-        
-        if (chatIdFromStore === null) { return; }
-
-        try {
-          fullChat = client.readFragment<FullChat>({
-            id: chatIdFromStore,
-            fragment: fragments.fullChat,
-            fragmentName: 'FullChat',
-          });
-        } catch (e) {
-          return;
-        }
-        
-        if (fullChat === null) { return; }
-        if (fullChat.messages.some((m:any) => m.id === addMessage.id)) return;
-
-        fullChat.messages.push(addMessage);
-        fullChat.lastMessage = addMessage;
-
-        client.writeFragment({
-          id: chatIdFromStore,
-          fragment: fragments.fullChat,
-          fragmentName: 'FullChat',
-          data: fullChat,
-        });
-
-        let data;
-        try {
-          data = client.readQuery<ChatsResult>({
-            query: queries.chats,
-          });
-        } catch (e) {
-          return;
-        }
-
-        if (!data) return;
-        const chats = data.chats;
-        if (!chats) return;
-
-        const chatIndex = chats.findIndex((c:any) => c.id === chatId);
-        if (chatIndex === -1) return;
-        const chatWhereAdded = chats[chatIndex];
-
-        // The chat will appear at the top of the ChatsList component
-        chats.splice(chatIndex, 1);
-        chats.unshift(chatWhereAdded);        
-
-        client.writeQuery({
-          query: queries.chats,
-          data: { chats: chats },
-        });
-      }
-    });
+        writeMessage(client, addMessage);
+      },
+    })
   }, [data, chatId, addMessage]);
 
   if (data === undefined) {
