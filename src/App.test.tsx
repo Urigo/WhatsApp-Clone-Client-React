5fd9838f1a3186b7f1c23814acbc6db0832b5b5a
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-27 17:19:09 +0200

    Step 10.6: Use cache service

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,9 +3,15 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { mockApolloClient } from './test-helpers';
+import * as subscriptions from './graphql/subscriptions';
 
 it('renders without crashing', () => {
-  const client = mockApolloClient();
+  const client = mockApolloClient([
+    {
+      request: { query: subscriptions.messageAdded },
+      result: { data: {} }
+    }
+  ]);
   const div = document.createElement('div');
 
   ReactDOM.render(
