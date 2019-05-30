59b84810022e5337f922b892a8ff5647598891f6
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-30 12:11:09 +0200

    Step 12.5: Update cache on chat removal

--- src/components/ChatRoomScreen/index.tsx
+++ src/components/ChatRoomScreen/index.tsx
@@ -1,6 +1,7 @@
 import gql from 'graphql-tag';
 import React from 'react';
 import { useCallback } from 'react';
+import { Redirect } from 'react-router-dom';
 import styled from 'styled-components';
 import ChatNavbar from './ChatNavbar';
 import MessageInput from './MessageInput';
@@ -94,6 +95,11 @@ const ChatRoomScreen: React.FC<ChatRoomScreenParams> = ({
 
   if (loadingChat) return null;
   if (chat === null) return null;
+
+  // Chat was probably removed from cache by the subscription handler
+  if (!chat) {
+    return <Redirect to="/chats" />;
+  }
 
   return (
     <Container>
