86f02505034dba5976a5a7f3b5791489eaa0fcf7
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-30 12:29:42 +0200

    Step 13.2: Update auth service to call signIn mutation

---
+++ src/graphql/mutations/signIn.mutation.ts
@@ -0,0 +1,9 @@
+import gql from 'graphql-tag'
+
+export default gql`
+  mutation signIn($username: String!, $password: String!) {
+    signIn(username: $username, password: $password) {
+      id
+    }
+  }
+`
