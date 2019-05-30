3a16f2a7b070a8db64f42a3fdc803e4146b99418
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-30 11:57:17 +0200

    Step 12.4: Add chat removal function

--- src/components/ChatRoomScreen/ChatNavbar.test.tsx
+++ src/components/ChatRoomScreen/ChatNavbar.test.tsx
@@ -1,12 +1,17 @@
 import { createMemoryHistory } from 'history';
 import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
 import { cleanup, render, waitFor, fireEvent } from '@testing-library/react';
+import { mockApolloClient } from '../../test-helpers';
 import ChatNavbar from './ChatNavbar';
+import { RemoveChatDocument } from '../../graphql/types';
 
 describe('ChatNavbar', () => {
   afterEach(cleanup);
 
   it('renders chat data', () => {
+    const client = mockApolloClient();
+
     const time = new Date('1 Jan 2019 GMT');
     const chat = {
       id: '1',
@@ -30,7 +35,9 @@ describe('ChatNavbar', () => {
 
     {
       const { container, getByTestId } = render(
-        <ChatNavbar chat={chat} history={history} />
+        <ApolloProvider client={client}>
+          <ChatNavbar chat={chat} history={history} />
+        </ApolloProvider>
       );
 
       expect(getByTestId('chat-name')).toHaveTextContent('Foo Bar');
@@ -42,6 +49,8 @@ describe('ChatNavbar', () => {
   });
 
   it('goes back on arrow click', async () => {
+    const client = mockApolloClient();
+
     const time = new Date('1 Jan 2019 GMT');
     const chat = {
       id: '1',
@@ -69,10 +78,65 @@ describe('ChatNavbar', () => {
 
     {
       const { container, getByTestId } = render(
-        <ChatNavbar chat={chat} history={history} />
+        <ApolloProvider client={client}>
+          <ChatNavbar chat={chat} history={history} />
+        </ApolloProvider>
       );
 
       fireEvent.click(getByTestId('back-button'));
+
+      await waitFor(() => expect(history.location.pathname).toEqual('/chats'));
+    }
+  });
+
+  it('goes back on chat removal', async () => {
+    const client = mockApolloClient([
+      {
+        request: {
+          query: RemoveChatDocument,
+          variables: { chatId: '1' },
+        },
+        result: {
+          data: {
+            removeChat: '1',
+          },
+        },
+      },
+    ]);
+
+    const time = new Date('1 Jan 2019 GMT');
+    const chat = {
+      id: '1',
+      name: 'Foo Bar',
+      picture: 'https://localhost:4000/picture.jpg',
+      messages: [
+        {
+          id: '1',
+          content: 'foo',
+          createdAt: time,
+        },
+        {
+          id: '2',
+          content: 'bar',
+          createdAt: time,
+        },
+      ],
+    };
+
+    const history = createMemoryHistory();
+
+    history.push('/chats/1');
+
+    await waitFor(() => expect(history.location.pathname).toEqual('/chats/1'));
+
+    {
+      const { container, getByTestId } = render(
+        <ApolloProvider client={client}>
+          <ChatNavbar chat={chat} history={history} />
+        </ApolloProvider>
+      );
+
+      fireEvent.click(getByTestId('delete-button'));
 
       await waitFor(() => expect(history.location.pathname).toEqual('/chats'));
     }
