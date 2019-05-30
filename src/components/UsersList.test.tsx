a05be593a75e5b18f489b081500c095926128d08
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-30 11:43:19 +0200

    Step 12.2: Create chat on user pick

--- src/components/UsersList.test.tsx
+++ src/components/UsersList.test.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { cleanup, render, waitForDomChange } from '@testing-library/react';
+import {
+  cleanup,
+  render,
+  fireEvent,
+  wait,
+  waitForDomChange,
+} from '@testing-library/react';
 import { mockApolloClient } from '../test-helpers';
 import UsersList, { UsersListQuery } from './UsersList';
 import * as queries from '../graphql/queries';
@@ -39,6 +45,47 @@ describe('UsersList', () => {
       expect(getByTestId('name')).toHaveTextContent('Charles Dickhead');
       expect(getByTestId('picture')).toHaveAttribute(
         'src',
+        'https://localhost:4000/dick.jpg'
+      );
+    }
+  });
+
+  it('triggers onUserPick() callback on user-item click', async () => {
+    const client = mockApolloClient([
+      {
+        request: { query: UsersListQuery },
+        result: {
+          data: {
+            users: [
+              {
+                __typename: 'User',
+                id: 1,
+                name: 'Charles Dickhead',
+                picture: 'https://localhost:4000/dick.jpg',
+              },
+            ],
+          },
+        },
+      },
+    ]);
+
+    const onUserPick = jest.fn(() => {});
+
+    {
+      const { container, getByTestId } = render(
+        <ApolloProvider client={client}>
+          <UsersList onUserPick={onUserPick} />
+        </ApolloProvider>
+      );
+
+      await waitForDomChange({ container });
+
+      fireEvent.click(getByTestId('user'));
+
+      await wait(() => expect(onUserPick.mock.calls.length).toBe(1));
+
+      expect(onUserPick.mock.calls[0][0].name).toEqual('Charles Dickhead');
+      expect(onUserPick.mock.calls[0][0].picture).toEqual(
         'https://localhost:4000/dick.jpg'
       );
     }
