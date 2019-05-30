d0a16ebfa580b7b81727a139ec92bfb7ca16ca38
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-30 12:32:23 +0200

    Step 13.3: Validate auth token against the back-end on restricted route

--- src/services/auth.service.tsx
+++ src/services/auth.service.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { useCallback } from 'react';
+import { useContext, useCallback } from 'react';
 import { useApolloClient } from '@apollo/react-hooks';
 import { Redirect } from 'react-router-dom';
-import { useSignInMutation } from '../graphql/types';
+import { useMeQuery, User, useSignInMutation } from '../graphql/types';
 import { useCacheService } from './cache.service';
+
+const MyContext = React.createContext<User | null>(null);
+
+export const useMe = () => {
+  return useContext(MyContext);
+};
 
 export const withAuth = <P extends object>(
   Component: React.ComponentType<P>
@@ -17,9 +23,26 @@ export const withAuth = <P extends object>(
       return <Redirect to="/sign-in" />;
     }
 
+    const signOut = useSignOut();
+    const { data, error, loading } = useMeQuery();
+
     useCacheService();
 
-    return <Component {...(props as P)} />;
+    if (loading) return null;
+
+    if (data === undefined) return null;
+
+    if (error || !data.me) {
+      signOut();
+
+      return <Redirect to="/sign-in" />;
+    }
+
+    return (
+      <MyContext.Provider value={data.me}>
+        <Component {...(props as P)} />
+      </MyContext.Provider>
+    );
   };
 };
 
