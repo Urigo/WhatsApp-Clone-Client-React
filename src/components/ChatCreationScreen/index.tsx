86646cd17fcfd83b64bd2b96adabbce4095aeeb7
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-30 11:43:19 +0200

    Step 12.2: Create chat on user pick

--- src/components/ChatCreationScreen/index.tsx
+++ src/components/ChatCreationScreen/index.tsx
@@ -1,8 +1,12 @@
+import gql from 'graphql-tag';
 import React from 'react';
+import { useCallback } from 'react';
 import styled from 'styled-components';
+import * as fragments from '../../graphql/fragments';
 import UsersList from '../UsersList';
 import ChatCreationNavbar from './ChatCreationNavbar';
 import { History } from 'history';
+import { useAddChatMutation } from '../../graphql/types';
 
 // eslint-disable-next-line
 const Container = styled.div`
@@ -15,15 +19,52 @@ const StyledUsersList = styled(UsersList)`
   height: calc(100% - 56px);
 `;
 
+gql`
+  mutation AddChat($recipientId: ID!) {
+    addChat(recipientId: $recipientId) {
+      ...Chat
+    }
+  }
+  ${fragments.chat}
+`;
+
 interface ChildComponentProps {
   history: History;
 }
 
-const ChatCreationScreen: React.FC<ChildComponentProps> = ({ history }) => (
-  <div>
-    <ChatCreationNavbar history={history} />
-    <UsersList />
-  </div>
-);
+const ChatCreationScreen: React.FC<ChildComponentProps> = ({ history }) => {
+  const [addChat] = useAddChatMutation();
+
+  const onUserPick = useCallback(
+    (user) =>
+      addChat({
+        optimisticResponse: {
+          __typename: 'Mutation',
+          addChat: {
+            __typename: 'Chat',
+            id: Math.random().toString(36).substr(2, 9),
+            name: user.name,
+            picture: user.picture,
+            lastMessage: null,
+          },
+        },
+        variables: {
+          recipientId: user.id,
+        },
+      }).then((result) => {
+        if (result && result.data !== null) {
+          history.push(`/chats/${result.data!.addChat!.id}`);
+        }
+      }),
+    [addChat, history]
+  );
+
+  return (
+    <div>
+      <ChatCreationNavbar history={history} />
+      <UsersList onUserPick={onUserPick} />
+    </div>
+  );
+};
 
 export default ChatCreationScreen;
