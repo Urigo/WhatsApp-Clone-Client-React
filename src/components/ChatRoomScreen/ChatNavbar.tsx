6fe238d3de5ed05bc39cf6963066a3f8f5f45265
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-30 12:11:09 +0200

    Step 12.5: Update cache on chat removal

--- src/components/ChatRoomScreen/ChatNavbar.tsx
+++ src/components/ChatRoomScreen/ChatNavbar.tsx
@@ -8,6 +8,7 @@ import { useCallback } from 'react';
 import styled from 'styled-components';
 import { History } from 'history';
 import { useRemoveChatMutation } from '../../graphql/types';
+import { eraseChat } from '../../services/cache.service';
 
 const Container = styled(Toolbar)`
   padding: 0;
@@ -66,6 +67,11 @@ const ChatNavbar: React.FC<ChatNavbarProps> = ({ chat, history }) => {
   const [removeChat] = useRemoveChatMutation({
     variables: {
       chatId: chat.id,
+    },
+    update: (client, { data }) => {
+      if (data && data.removeChat) {
+        eraseChat(client, data.removeChat);
+      }
     },
   });
 
