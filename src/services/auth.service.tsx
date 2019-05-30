5270fb08eafe0efe033ff392f520659fa17b8b0d
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-30 12:32:23 +0200

    Step 13.3: Validate auth token against the back-end on restricted route

--- src/services/auth.service.tsx
+++ src/services/auth.service.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { useContext } from 'react';
 import { Redirect } from 'react-router-dom';
 import client from '../client';
+import { useMeQuery, User } from '../graphql/types';
 import { useCacheService } from './cache.service';
 import gql from 'graphql-tag';
+
+const MyContext = React.createContext<User|null>(null);
+
+export const useMe = () => {
+  return useContext(MyContext);
+};
 
 export const withAuth = <P extends object>(Component: React.ComponentType<P>) => {
   return (props: any) => {
@@ -16,10 +24,24 @@ export const withAuth = <P extends object>(Component: React.ComponentType<P>) =>
       );
     }
 
+    const { data, error, loading } = useMeQuery();
+
     useCacheService();
 
+    if (loading) return null;
+
+    if (data === undefined) return null;
+
+    if (error || !data.me) {
+      signOut();
+
+      return <Redirect to="/sign-in" />;
+    }
+
     return (
+      <MyContext.Provider value={data.me}>
         <Component {...props as P} />
+      </MyContext.Provider>
     );
   };
 };
