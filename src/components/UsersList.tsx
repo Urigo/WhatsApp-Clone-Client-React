d2bf571b6a23491e213bdfc61d1cbabe3c32c824
Uri Goldshtein <uri.goldshtein@gmail.com>
2019-05-30 11:43:19 +0200

    Step 12.2: Create chat on user pick

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -4,7 +4,7 @@ import gql from 'graphql-tag';
 import React from 'react';
 import styled from 'styled-components';
 import * as fragments from '../graphql/fragments';
-import { useUsersListQuery } from '../graphql/types';
+import { useUsersListQuery, User } from '../graphql/types';
 
 const ActualList = styled(MaterialList) `
   padding: 0;
@@ -38,7 +38,11 @@ export const UsersListQuery = gql`
   ${fragments.user}
 `;
 
-const UsersList: React.FC = () => {
+interface ChildComponentProps {
+  onUserPick: any;
+};
+
+const UsersList: React.FC<ChildComponentProps> = ({ onUserPick = (user: User) => {} }) => {
   const { data, loading: loadingUsers } = useUsersListQuery();
 
   if (data === undefined) return null;
@@ -49,6 +53,8 @@ const UsersList: React.FC = () => {
       {!loadingUsers && users.map(user => (
         <UserItem
           key={user.id}
+          data-testid="user"
+          onClick={onUserPick.bind(null, user)}
           button
         >
           {(user !== null && user.picture !== null) &&
