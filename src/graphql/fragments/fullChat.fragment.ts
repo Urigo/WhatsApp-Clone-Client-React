f7d79d6462b1dda60a3fe544f66ef75370ec7055
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-06-03 10:52:38 +0200

    Step 15.6: Apply MessagesResult type

--- src/graphql/fragments/fullChat.fragment.ts
+++ src/graphql/fragments/fullChat.fragment.ts
@@ -1,14 +1,14 @@
 import gql from 'graphql-tag';
 import chat from './chat.fragment';
-import message from './message.fragment';
+import messagesResult from './messagesResult.fragment';
 
 export default gql`
   fragment FullChat on Chat {
     ...Chat
-    messages {
-      ...Message
+    messages(limit: $limit, after: $after) @connection(key: "messages") {
+      ...MessagesResult
     }
   }
   ${chat}
-  ${message}
+  ${messagesResult}
 `;
